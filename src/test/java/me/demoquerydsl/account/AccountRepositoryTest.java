0bba4195e487793e9a4183cfb1b586d626453571
sombrero104 <sombrero.khy@gmail.com>
2021-03-29 23:30:20 +0900

    .

--- src/test/java/me/demoquerydsl/account/AccountRepositoryTest.java
+++ src/test/java/me/demoquerydsl/account/AccountRepositoryTest.java
@@ -1,9 +1,13 @@
 package me.demoquerydsl.account;
 
+import com.querydsl.core.types.Predicate;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -14,7 +18,12 @@ class AccountRepositoryTest {
 
     @Test
     public void crud() {
-
+        QAccount account = QAccount.account;
+        Predicate predicate = account
+                .firstName.containsIgnoreCase("Erlend")
+                .and(account.lasName.startsWith("Oye"));
+        Optional<Account> one = accountRepository.findOne(predicate);
+        assertThat(one).isEmpty();
     }
 
 }
